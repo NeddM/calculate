260e7eec24b35429065cef1e32ba14f87a02f7e7
Nedd Chairi Muñoz <neddchairiweb@gmail.com>
2023-12-07 14:52:11 +0100

    🚀 Initial commit!

---
+++ lua/evaluate/init.lua
@@ -0,0 +1,18 @@
+local M = {}
+
+
+M.evaluate = function()
+  vim.ui.input({ prompt = "Type the math operation: " }, function(operation)
+    local success, result = pcall(vim.fn.eval, operation)
+    if success then
+      print(" = " .. result)
+    else
+      print("Error evaluating operation: " .. result)
+      vim.notify("Error: " .. result, "error")
+    end
+  end)
+end
+
+vim.cmd([[command! -nargs=0 Evaluate lua require('evaluate').evaluate()]])
+
+return M
