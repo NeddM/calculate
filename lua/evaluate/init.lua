431d962416c1a5597ec1d32545a1a1f6e83268cf
Nedd Chairi Muñoz <neddchairiweb@gmail.com>
2023-12-07 15:29:33 +0100

    🛠️ Testing method

--- lua/evaluate/init.lua
+++ lua/evaluate/init.lua
@@ -1,3 +1,27 @@
+-- local M = {}
+
+-- M.evaluate = function()
+--   vim.ui.input({ prompt = "Type the math operation: " }, function(operation)
+--     local success, result = pcall(vim.fn.eval, operation)
+--     if success then
+--       print(" = " .. result)
+--     else
+--       print("Error evaluating operation: " .. result)
+--       vim.notify("Error: " .. result, "error")
+--     end
+--   end)
+-- end
+
+
+-- M.setup = function()
+--   vim.cmd([[command! -nargs=0 Evaluate 'lua require("evaluate").evaluate()']])
+-- end
+
+
+-- return M
+
+
+
 local M = {}
 
 M.evaluate = function()
@@ -12,10 +36,9 @@ M.evaluate = function()
   end)
 end
 
-
 M.setup = function()
+  require("evaluate")
   vim.cmd([[command! -nargs=0 Evaluate 'lua require("evaluate").evaluate()']])
 end
-
 
 return M
